acc50b1994a6ec87c954259178d76b9282925c07
Hakizu <hakisuj@gmail.com>
2020-06-22 14:22:12 +0200

    Completed Follow Along 5.b

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './Components/Note'
 import notesServices from './services/notes'
 import Notification from './Components/Notification'
@@ -7,138 +7,138 @@ import LoginForm from './Components/login'
 import Togglable from './Components/Togglable'
 import NoteForm from './Components/NoteForm'
 
-const App = (props) => {
-    const [notes, setNotes] = useState([])
-    const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState(null)
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [user, setUser] = useState(null)
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
-    const handleLogin = async (event) => {
-        event.preventDefault()
-        try {
-            const user = await loginService.login({
-                username, password,
-        })
-        window.localStorage.setItem(
-            'loggedNoteappUser', JSON.stringify(user)
-        )
-        
-        notesServices.setToken(user.token)
-        setUser(user)
-        setUsername('')
-        setPassword('')
-        } catch (exception) {
-            setErrorMessage('Wrong credentials')
-            setTimeout(() => {
-                setErrorMessage(null)
-            }, 5000)
-        }
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+
+      notesServices.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
+  }
 
-    useEffect (() => {
-        notesServices
-            .getAll()
-            .then(response => {
-                setNotes(response)
-        })
-    }, [])
+  useEffect (() => {
+    notesServices
+      .getAll()
+      .then(response => {
+        setNotes(response)
+      })
+  }, [])
 
-    useEffect(() => {
-        const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
-        if (loggedUserJSON) {
-            const user = JSON.parse(loggedUserJSON)
-            setUser(user)
-            notesServices.setToken(user.token)
-        }
-    }, [])
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      notesServices.setToken(user.token)
+    }
+  }, [])
 
-    const toggleImportanceOf = (id) => {
-        const note = notes.find(n => n.id === id)
-        const changedNote = {...note, important: !note.important}
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
-        notesServices
-            .update(id, changedNote)
-            .then(response => {
-                setNotes(notes.map(note => 
-                    note.id !== id ? note : response))
-            })  
-            .catch(error => {
-                setErrorMessage(`the note '${note.content}' was already 
+    notesServices
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note =>
+          note.id !== id ? note : response))
+      })
+      .catch(() => {
+        setErrorMessage(`the note '${note.content}' was already 
                 deleted from server`)
-            setTimeout(() => {
-                setErrorMessage(null)
-            }, 10000)
-            })
-    }
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 10000)
+      })
+  }
 
-    const notesToShow = showAll 
-        ? notes 
-        : notes.filter(note => note.important)
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
 
-    const addNote =(noteObject) => {
-        noteFormRef.current.toggleVisibiliity()
-        notesServices
-            .create(noteObject)
-            .then(returnedNote => {
-                setNotes(notes.concat(returnedNote))
-            })
-    }
+  const addNote =(noteObject) => {
+    noteFormRef.current.toggleVisibiliity()
+    notesServices
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+      })
+  }
 
-    const noteFormRef = React.createRef()
+  const noteFormRef = React.createRef()
 
-    const noteForm = () => (
-        <Togglable buttonLabel='New Note' ref={noteFormRef}>
-            <NoteForm createNote = {addNote}/>
-        </Togglable>
-    )
+  const noteForm = () => (
+    <Togglable buttonLabel='New Note' ref={noteFormRef}>
+      <NoteForm createNote = {addNote}/>
+    </Togglable>
+  )
 
-    const loginForm = () => (
-        <Togglable buttonLabel='login'>
-            <LoginForm
-                username = {username}
-                password = {password}
-                handleUsernameChange = {({ target }) => setUsername(target.value)}
-                handlePasswordChange = {({ target }) => setPassword(target.value)}
-                handleSubmit = {handleLogin}
-            />
-        </Togglable>
-    )
-    
-    return (
+  const loginForm = () => (
+    <Togglable buttonLabel='login'>
+      <LoginForm
+        username = {username}
+        password = {password}
+        handleUsernameChange = {({ target }) => setUsername(target.value)}
+        handlePasswordChange = {({ target }) => setPassword(target.value)}
+        handleSubmit = {handleLogin}
+      />
+    </Togglable>
+  )
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <Notification message={errorMessage}/>
+
+      {user === null ?
+        loginForm():
         <div>
-        <h1>Notes</h1>
-
-        <Notification message={errorMessage}/>
-        
-        {user === null ?
-            loginForm():
-            <div>
-                <p>{user.name} logged in</p>
-                {noteForm()}
-            </div>
-        }
-
-        <div className='show'>
-            <button onClick={() => setShowAll(!showAll)}>
-                show{showAll ? ' important' : ' all'}
-            </button>
+          <p>{user.name} logged in</p>
+          {noteForm()}
         </div>
+      }
 
-        <table><tbody>
-          {notesToShow.map((note, i) => 
-            <Note 
-                key={i} 
-                note={note} 
-                toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-          )}
-        </tbody></table>
-
+      <div className='show'>
+        <button onClick={() => setShowAll(!showAll)}>
+                show{showAll ? ' important' : ' all'}
+        </button>
       </div>
-    )
+
+      <table><tbody>
+        {notesToShow.map((note, i) =>
+          <Note
+            key={i}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </tbody></table>
+
+    </div>
+  )
 }
 
 export default App
--- src/Components/login.js
+++ src/Components/login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
   handleSubmit,
@@ -7,7 +8,7 @@ const LoginForm = ({
   username,
   password,
 }) => {
-  
+
   return (
     <div>
       <h2>Login</h2>
@@ -27,11 +28,19 @@ const LoginForm = ({
             value={password}
             onChange={handlePasswordChange}
           />
-      </div>
+        </div>
         <button type="submit">login</button>
       </form>
     </div>
   )
+}
+
+LoginForm.propTypes = {
+  handleSubmit:PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password:PropTypes.string.isRequired,
 }
 
 export default LoginForm
